609d6b24a9d98c8309375b5a55928ad381d7fd78
MolinaCastellanos <molina.castellanos.nisa.izel2022@gmail.com>
2024-09-11 19:38:26 -0600

    primer versión

---
+++ validar.js
@@ -0,0 +1,34 @@
+function validar(form) {
+
+    let nombre = form.nombre.value.trim()
+
+    if (nombre == "") {
+        alert("Por favor, ingrese su nombre.");
+        return false;
+    }
+
+    let edad = form.edad.value.trim()
+
+    if (edad === "" || isNaN(edad) || edad < 14) {
+        alert("Por favor, ingrese una edad válida")
+        return false;
+    }
+
+    let sexo=form.value
+    if(sexo==""){
+        alert("Por favor, selecciona un sexo")
+        return false;
+
+    }
+
+    let deporte=form.deporte.value
+    if(deporte==="seleccione"){
+        alert("Por favor, seleccione un deporte favorito")
+        return false;
+    }
+
+//Si todo es valido
+
+    return true;
+
+}
